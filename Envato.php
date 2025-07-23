3a4e98964bfd493b1f646fafb0f02804cbfd8292
Y4SIIIIN <86015580+Y4SIIIIN@users.noreply.github.com>
2025-07-23 10:06:55 -0700

    CSRF
    I'm you times two

--- Envato.php
+++ Envato.php
@@ -62,11 +62,34 @@ if(isFind($link, 'elements.envato.com')) {
       'sec-fetch-site: same-origin',
       'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
   );
-# WHOSE NAME IS THE VOICE IN YOUR EARS ?	
+# WHOSE NAME IS THE VOICE IN YOUR EARS ?	  
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_HEADER, true);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie);
+    curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie);
 
-	
-	
-	
+    $response = curl_exec($ch);
+    $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+    $header = substr($response, 0, $header_size);
+
+
+    preg_match('/^x-csrf-token:\s*([^\r\n]+)/mi', $header, $matches);
+    $csrf = $matches[1] ?? null;
+
+    preg_match('/^x-csrf-token-2:\s*([^\r\n]+)/mi', $header, $matches2);
+    $csrf2 = $matches2[1] ?? null;
+    
+    $item = $_GET['url'];
+    if(isFind($item, '-')) {
+       $item = explode('-', $item);
+       $item = $item[(sizeof($item) - 1)];
+   }	
+# BEAUTY CAN ONLY BE SEEN IN A MIRROR. WE CREATED EVERYTHING IN PAIRS. LOOK INTO YOUR OWN EYES.	
 	
 	$ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
